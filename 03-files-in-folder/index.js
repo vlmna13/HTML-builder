5c779dbb82760d103d090dda3c88916634f599b7
vlmna13 <vlmna13@gmail.com>
2025-01-03 16:20:26 +0300

    feat: add sollution for task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const wayToFile = path.resolve(__dirname, 'secret-folder');
+fs.readdir(wayToFile, { withFileTypes: true }, (err, files) => {
+    if(err) {
+        console.log(err.message);
+    } else {
+        files.forEach((file) => {
+            if (file.isFile()) {
+              const filePath = path.join(file.path, file.name);
+              const extension = path.parse(filePath).ext.slice(1);
+              const name = path.parse(filePath).name;
+              fs.stat(filePath, (err, stats) => {
+                const fileSize = stats.size;
+                console.log(`${name} - ${extension} - ${fileSize}kb`);
+              });
+            }
+        });
+    }
+});
