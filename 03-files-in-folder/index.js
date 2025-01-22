1a7e0ab81e5ad0c2fe74850eecbd682298013007
vlmna13 <vlmna13@gmail.com>
2025-01-22 15:05:11 +0300

    fix: fix 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -11,7 +11,7 @@ fs.readdir(wayToFile, { withFileTypes: true }, (err, files) => {
               const extension = path.parse(filePath).ext.slice(1);
               const name = path.parse(filePath).name;
               fs.stat(filePath, (err, stats) => {
-                const fileSize = stats.size;
+                const fileSize = (stats.size / 1024).toFixed(2);
                 console.log(`${name} - ${extension} - ${fileSize}kb`);
               });
             }
